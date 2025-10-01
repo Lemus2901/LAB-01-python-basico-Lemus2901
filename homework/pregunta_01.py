8d8aa691cbde9811e49ef82bac4ef193ee707292
Andres Felipe Lemus Victoria <164676691+Lemus2901@users.noreply.github.com>
2025-10-01 11:22:26 -0500

    Implement function to sum values in second column

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -5,12 +5,19 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
-
 def pregunta_01():
     """
     Retorne la suma de la segunda columna.
 
     Rta/
     214
-
     """
+    suma = 0
+    with open("data.csv", "r") as file:
+        for line in file:
+            # Quitar saltos de línea y separar por tabulador
+            columnas = line.strip().split("\t")
+            
+            # Tomar el valor de la segunda columna (índice 1) y sumarlo
+            suma += int(columnas[1])
+    return suma
